0aecae1f65e7b460730946daee6efdf2282ac2ed
rafael <rafaelzapico.ti@gmail.com>
2024-09-03 12:52:48 -0300

    correção animação staggered

--- lib/view/pages/staggered_animation.dart
+++ lib/view/pages/staggered_animation.dart
@@ -1,9 +1,4 @@
 import 'dart:math';
-
-import 'package:animations/view/layout/appbar.dart';
-import 'package:animations/view/layout/menu.dart';
-import 'package:animations/view/pages/container.dart';
-import 'package:animations/view/pages/hero_begin.dart';
 import 'package:flutter/material.dart';
 
 class StaggeredAnimation extends StatefulWidget {
@@ -18,14 +13,14 @@ class StaggeredAnimationState extends State<StaggeredAnimation>
   late AnimationController _animationController;
   late List<Animation<double>> _starScaleAnimations;
   late List<Animation<double>> _starJumpAnimations;
-  late List<Animation<double>> _starRotateAnimations;
+  late List<Animation<double>> _starRotationAnimations;
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 1105),
+      duration: const Duration(milliseconds: 2000),
     );
 
     _starScaleAnimations = List.generate(5, (index) {
@@ -51,10 +46,21 @@ class StaggeredAnimationState extends State<StaggeredAnimation>
         ),
         TweenSequenceItem(
           tween: Tween<double>(begin: -40, end: 0)
-              .chain(CurveTween(curve: Curves.easeIn)),
+              .chain(CurveTween(curve: Curves.easeOutSine)),
           weight: 50,
         ),
       ]).animate(
+        CurvedAnimation(
+          parent: _animationController,
+          curve: Interval(start, end, curve: Curves.easeOut),
+        ),
+      );
+    });
+
+    _starRotationAnimations = List.generate(5, (index) {
+      final start = index * 0.2;
+      final end = start + 0.2;
+      return Tween<double>(begin: 0, end: 1).animate(
         CurvedAnimation(
           parent: _animationController,
           curve: Interval(start, end, curve: Curves.easeOut),
@@ -73,8 +79,12 @@ class StaggeredAnimationState extends State<StaggeredAnimation>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 205, 206, 206),
-      appBar: CustomAppBar(nome: "Staggered Animations"),
-      drawer: const MenuDrawer(),
+      appBar: AppBar(
+        title: const Text("Staggered Animations"),
+        backgroundColor: Colors.indigoAccent,
+        foregroundColor: Colors.white,
+      ),
+      drawer: const Drawer(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -87,26 +97,49 @@ class StaggeredAnimationState extends State<StaggeredAnimation>
               child: Center(
                 child: Padding(
                   padding: const EdgeInsets.all(18.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: List.generate(5, (index) {
-                      return AnimatedBuilder(
-                        animation: _animationController,
-                        builder: (context, child) {
-                          return Transform.translate(
-                            offset: Offset(0, _starJumpAnimations[index].value),
-                            child: Transform.scale(
-                              scale: _starScaleAnimations[index].value,
-                              child: const Icon(
-                                Icons.star_rounded,
-                                size: 60,
-                                color: Colors.amber,
-                              ),
-                            ),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: List.generate(5, (index) {
+                          return const Icon(
+                            Icons.star_rounded,
+                            size: 60,
+                            color: Color.fromARGB(255, 138, 162, 173),
                           );
-                        },
-                      );
-                    }),
+                        }),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: List.generate(5, (index) {
+                          return AnimatedBuilder(
+                            animation: _animationController,
+                            builder: (context, child) {
+                              return Transform.translate(
+                                offset:
+                                    Offset(0, _starJumpAnimations[index].value),
+                                child: Transform.scale(
+                                  scale: _starScaleAnimations[index].value,
+                                  child: Transform.rotate(
+                                    angle:
+                                        _starRotationAnimations[index].value *
+                                            4 *
+                                            pi,
+                                    child: const Icon(
+                                      Icons.star_rounded,
+                                      size: 60,
+                                      color: Colors
+                                          .amber, // Cor das estrelas animadas
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }),
+                      ),
+                    ],
                   ),
                 ),
               ),
